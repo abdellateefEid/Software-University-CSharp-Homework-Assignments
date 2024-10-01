13d0958034c4ec701bd4da33c21187e49b8718cd
Abdellateef Eid <abdellateef35@gmail.com>
2024-10-01 15:19:04 +0300

    initial commit

---
+++ 01. CSharp-Advanced-Stacks-And-Queues/Stacks-And-Queues-Lab/Stacks-And-Queues-Lab/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Stacks_And_Queues_Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
