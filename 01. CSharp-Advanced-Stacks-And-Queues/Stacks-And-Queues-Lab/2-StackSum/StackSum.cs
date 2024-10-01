533d38c5c07e5d703d299149b31e2d602dd8fdf9
Abdellateef Eid <abdellateef35@gmail.com>
2024-10-01 21:00:26 +0300

    StackSum done

--- 01. CSharp-Advanced-Stacks-And-Queues/Stacks-And-Queues-Lab/2-StackSum/StackSum.cs
+++ 01. CSharp-Advanced-Stacks-And-Queues/Stacks-And-Queues-Lab/2-StackSum/StackSum.cs
@@ -4,6 +4,60 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("enter arr nums: ");
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> stack = new Stack<int>();
+            foreach (int num in arr)
+            {
+                stack.Push(num);
+            }
+
+            Console.WriteLine("enter your commands: ");
+
+
+            bool flag = true;
+
+            while (flag)
+            {
+                string[] fullCommand = Console.ReadLine().Split();
+
+                string command = fullCommand[0].ToLower();
+
+                switch (command)
+                {
+                    case "add":
+                        stack.Push(int.Parse(fullCommand[1]));
+                        stack.Push(int.Parse(fullCommand[2]));
+                        break;
+
+
+                    case "remove":
+                        int count = int.Parse(fullCommand[1]);
+                        if (count <= stack.Count)
+                        {
+                            while (count > 0)
+                            {
+                                stack.Pop();
+                                count--;
+                            }
+                        }
+                        break;
+                    case "end":
+                        int sum = 0;
+                        while (stack.Count() > 0)
+                        {
+                            sum += stack.Pop();
+                        }
+
+                        Console.WriteLine($"Sum: {sum}");
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("enter valid command");
+                        break;
+                }
+            }
 
         }
     }
