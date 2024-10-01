0d7e84f6ffb07ebd45013b3f6b3e0023fe2f614b
Abdellateef Eid <abdellateef35@gmail.com>
2024-10-01 21:55:16 +0300

    SimpleCalculator done

--- 01. CSharp-Advanced-Stacks-And-Queues/Stacks-And-Queues-Lab/3-SimpleCalc/SimpleCalc.cs
+++ 01. CSharp-Advanced-Stacks-And-Queues/Stacks-And-Queues-Lab/3-SimpleCalc/SimpleCalc.cs
@@ -4,7 +4,69 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("enter your expressions:");
+
+            string[] expression = Console.ReadLine().Split();
+
+            #region Without Stack
+            /*
+            //2 + 5 + 10 - 2 - 1     // 14
+            int result = int.Parse(expression[0]);
+
+            for (int i = 1; i < expression.Length - 1; i++)
+            {
+                if (expression[i] == "+")
+                {
+                    result += int.Parse(expression[i + 1]);
+                }
+                else if (expression[i] == "-")
+                {
+                    result -= int.Parse(expression[i + 1]);
+                }
+            }
+
+            Console.WriteLine(result);
+            */
+            #endregion
+
+
+            #region Using 2stacks (one for operators and another for numbers)
+
+            Stack<int> stackOfNums = new Stack<int>();
+            Stack<string> stackOfOprators = new Stack<string>();
+
+            for (int i = expression.Length - 1; i >= 0; i--)
+            {
+                if (int.TryParse(expression[i], out int num))
+                {
+                    stackOfNums.Push(num);
+                }
+                else
+                {
+                    stackOfOprators.Push(expression[i]);
+                }
+            }
+
+            //2 + 5 + 10 - 2 - 1     // 14
+
+            int result = stackOfNums.Pop();
+
+            while (stackOfOprators.Count > 0)
+            {
+                if (stackOfOprators.Peek() == "+")
+                {
+                    result += stackOfNums.Pop();
+                }
+                else if (stackOfOprators.Peek() == "-")
+                {
+                    result -= stackOfNums.Pop();
+                }
+                stackOfOprators.Pop();
+            }
+
+            Console.WriteLine(result);
+
+            #endregion
         }
     }
 }
