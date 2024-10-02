b6d7130ca7bd22ec1abef27b0901739acf3ec1fd
Abdellateef Eid <abdellateef35@gmail.com>
2024-10-02 21:10:32 +0300

    PrintEvenNums done

---
+++ 01. CSharp-Advanced-Stacks-And-Queues/Stacks-And-Queues-Lab/PrintEvenNums/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace PrintEvenNums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter nums:");
+
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> queue = new Queue<int>(nums);
+
+            while (queue.Count > 0)
+            {
+                if (queue.Peek() % 2 == 0)
+                {
+                    Console.Write(queue.Peek() + ", ");
+                }
+
+                queue.Dequeue();
+            }
+        }
+    }
+}
